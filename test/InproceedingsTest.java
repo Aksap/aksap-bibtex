f4b7dfc2adb993bec1a2caf6c91f0ad172658f59
Juhani Leppänen <juhani.leppanen@cs.helsinki.fi>
2013-09-25 22:07:38 +0300

    Inproceedings-luokan testit poistettu toistaiseksi

--- test/InproceedingsTest.java
+++ test/InproceedingsTest.java
@@ -5,15 +5,4 @@ import static org.junit.Assert.assertEquals;
 
 public class InproceedingsTest {
 
-    @Test
-    public void testInproceedings1() throws Exception {
-
-        Inproceedings inproceedings = new Inproceedings("Arto Vihavainen", "Kukkien kastelu ja web-kehitys", "Vastaus kaikkiin maailman kysymyksiin", 2011);
-
-        assertEquals("Arto Vihavainen", inproceedings.getAuthor());
-        assertEquals("Kukkien kastelu ja web-kehitys", inproceedings.getTitle());
-        assertEquals("Vastaus kaikkiin maailman kysymyksiin", inproceedings.getBooktitle());
-        assertEquals(2011, inproceedings.getYear());
-
-    }
 }
